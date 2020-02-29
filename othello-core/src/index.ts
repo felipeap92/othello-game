ff750d02ba59b2c68ba33521f539dce6cecdbe53
Felipe de Assis Pinto <felipeap92@yahoo.com.br>
2020-02-29 10:54:33 -0300

    Update azure-pipelines.yml and index.ts

--- othello-core/src/index.ts
+++ othello-core/src/index.ts
@@ -2,4 +2,5 @@ import TicTacToeGame from './Game/tic-tac-toe-game';
 
 const ticTacToeGame = new TicTacToeGame();
 
+// tslint:disable-next-line: no-console
 console.log(JSON.stringify(ticTacToeGame.getBoard()));
